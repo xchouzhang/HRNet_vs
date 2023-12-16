797e9d903cb0f691fc729bf52c728c3d818bd1e0
zhangxp <fox_xchouzhang@163.com>
2023-12-16 22:40:40 +0800

    modidied smalltifs_predict.py

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -11,50 +11,82 @@ from tqdm import tqdm
 from utils.metric import runningScore
 
 
-def flip_inference(model, image, flip=False):
+def flip_inference(model, image, flip=False, with_edge=False):
+
     batch, _, ori_height, ori_width = image.size()
+
     assert batch == 1, "only supporting batchsize 1."
-    # image = image.numpy()[0].transpose((1, 2, 0)).copy()     #(512,512,4)
-    #
-    # final_pred = torch.zeros([1, self.num_classes,
-    #                           ori_height, ori_width]).cuda()
 
-    seg_pred, edge_pred = model(image)
+    
 
+    if not with_edge:
+
+        seg_pred = model(image)
+    
+    else:
+        seg_pred, edge_pred = model(image)
+
+    
     if flip:
-        # 水平翻转
-        hflip_img = image.cpu().numpy()[:, :, :, ::-1]      # size:[1,4,512,512]
-        seg_hflip_output, edge_hflip_output = model(torch.from_numpy(hflip_img.copy()).cuda())
+        
+        if not with_edge:
 
-        seg_hflip_pred = seg_hflip_output.data.cpu().numpy().copy()
-        edge_hflip_pred = edge_hflip_output.data.cpu().numpy().copy()
+            # 水平翻转
+            hflip_img = image.cpu().numpy()[:, :, :, ::-1]      # size:[1,4,512,512]
+            seg_hflip_output = model(torch.from_numpy(hflip_img.copy()).cuda())
 
-        seg_hflip_pred = torch.from_numpy(seg_hflip_pred[:, :, :, ::-1].copy()).cuda()
-        edge_hflip_pred = torch.from_numpy(edge_hflip_pred[:, :, :, ::-1].copy()).cuda()
-
-        seg_pred += seg_hflip_pred
-        edge_pred += edge_hflip_pred
-
-
-        # 垂直翻转
-        vflip_img = image.cpu().numpy()[:, :, ::-1, :]
-        seg_vflip_output, edge_vflip_output = model(torch.from_numpy(vflip_img.copy()).cuda())
+            seg_hflip_pred = seg_hflip_output.data.cpu().numpy().copy()
+            seg_hflip_pred = torch.from_numpy(seg_hflip_pred[:, :, :, ::-1].copy()).cuda()
+            seg_pred += seg_hflip_pred
+        
+            # 垂直翻转
+            vflip_img = image.cpu().numpy()[:, :, ::-1, :]
+            seg_vflip_output = model(torch.from_numpy(vflip_img.copy()).cuda())
 
 
-        seg_vflip_pred = seg_vflip_output.data.cpu().numpy().copy()
-        edge_vflip_pred = edge_vflip_output.data.cpu().numpy().copy()
+            seg_vflip_pred = seg_vflip_output.data.cpu().numpy().copy()
+            seg_vflip_pred = torch.from_numpy(seg_vflip_pred[:, :, ::-1, :].copy()).cuda()
+            seg_pred += seg_vflip_pred
+            
+            seg_pred = seg_pred / 3
 
-        seg_vflip_pred = torch.from_numpy(seg_vflip_pred[:, :, ::-1, :].copy()).cuda()
-        edge_vflip_pred = torch.from_numpy(edge_vflip_pred[:, :, ::-1, :].copy()).cuda()
+            return seg_pred
+        
+        else:
+            # 水平翻转
+            hflip_img = image.cpu().numpy()[:, :, :, ::-1]      # size:[1,4,512,512]
+            seg_hflip_output, edge_hflip_output = model(torch.from_numpy(hflip_img.copy()).cuda())
 
-        seg_pred += seg_vflip_pred
-        edge_pred += edge_vflip_pred
+            seg_hflip_pred = seg_hflip_output.data.cpu().numpy().copy()
+            edge_hflip_pred = edge_hflip_output.data.cpu().numpy().copy()
+
+            seg_hflip_pred = torch.from_numpy(seg_hflip_pred[:, :, :, ::-1].copy()).cuda()
+            edge_hflip_pred = torch.from_numpy(edge_hflip_pred[:, :, :, ::-1].copy()).cuda()
+
+            seg_pred += seg_hflip_pred
+            edge_pred += edge_hflip_pred
 
 
-        seg_pred = seg_pred / 3
-        edge_pred = edge_pred / 3
+            # 垂直翻转
+            vflip_img = image.cpu().numpy()[:, :, ::-1, :]
+            seg_vflip_output, edge_vflip_output = model(torch.from_numpy(vflip_img.copy()).cuda())
 
-    return seg_pred, edge_pred
+
+            seg_vflip_pred = seg_vflip_output.data.cpu().numpy().copy()
+            edge_vflip_pred = edge_vflip_output.data.cpu().numpy().copy()
+
+            seg_vflip_pred = torch.from_numpy(seg_vflip_pred[:, :, ::-1, :].copy()).cuda()
+            edge_vflip_pred = torch.from_numpy(edge_vflip_pred[:, :, ::-1, :].copy()).cuda()
+
+            seg_pred += seg_vflip_pred
+            edge_pred += edge_vflip_pred
+
+            seg_pred = seg_pred / 3
+            edge_pred = edge_pred / 3
+
+            return seg_pred, edge_pred
+
+        
 
 
 
---
+++ smalltifs_predict.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# @Company: Aerospace Information  Research Institute, Chinese Academy of Sciences
+# @Time :  
+# @Author : Xiaoping,Zhang
+# @File : predict.py
+
+
+
+import os
+import glob
+import numpy as np
+import torch
+import cv2
+import shutil
+
+import torchvision
+from dataset.datasets import read_tiff
+from config import default_config
+from config import update_config
+
+
+from evaluate.evaluate import evaluate, flip_inference
+
+
+def predict(cfg_file, best_model_path, test_imgs_path, pred_msks_path, save_edge = False, flip = False):
+
+    # device
+    device = torch.device('cuda'if torch.cuda.is_available() else 'cpu')
+
+    # model
+    if save_edge == True:
+        from Module.seg_hrnet_withedge import HighResolutionNet
+        net = HighResolutionNet(update_config(default_config, cfg_file), 4 ,1).to(device)
+    
+    else:
+        from Module.seg_hrnet import HighResolutionNet
+        net = HighResolutionNet(update_config(default_config, cfg_file), 4, 1).to(device)
+
+    # .pth load
+    net.load_state_dict(torch.load(best_model_path, map_location=device)['model_state'])
+
+
+    # make pred_mask_path
+    if not os.path.exists(pred_msks_path):
+        os.makedirs(pred_msks_path)
+        print("新建pred mask文件夹")
+    else:
+        shutil.rmtree(pred_msks_path)
+        print("删除已有pred mask文件夹")
+        os.makedirs(pred_msks_path)
+        print("重新新建pred mask文件夹")
+
+
+    # make pred_edges_path
+    if save_edge == True:
+        pred_edges_path = pred_msks_path.replace('msk_pred','edge_pred')
+
+        if not os.path.exists(pred_edges_path):
+            os.makedirs(pred_edges_path)
+            print("新建edge mask文件夹")
+        else:
+            shutil.rmtree(pred_edges_path)
+            print("删除已有edge mask文件夹")
+            os.makedirs(pred_edges_path)
+            print("重新新建edge mask文件夹")
+
+        
+
+    # each img path
+    test_imgs_path_name = [tuple([os.path.join(test_imgs_path, each), each]) for each in os.listdir(test_imgs_path)]
+    
+    print("---------------------------------------------------------")
+    print('predicting....')
+    print("---------------------------------------------------------")
+
+
+    # predict
+
+    for each_test_img_path, each_test_img_name in test_imgs_path_name:
+        
+        # read img
+        img = read_tiff(each_test_img_path) 
+        img = np.transpose(img,(1,2,0)) 
+
+        # to tensor
+        img_tensor = torchvision.transforms.ToTensor()(img)
+        img_tensor = torch.unsqueeze(img_tensor,dim = 0).to(device=device, dtype=torch.float32)
+
+
+        # each save path 
+        each_pred_msk_name = each_test_img_name
+        each_pred_msk_path = os.path.join(pred_msks_path, each_pred_msk_name)
+
+        if save_edge:
+            each_pred_edge_name = each_test_img_name
+            each_pred_edge_path = os.path.join(pred_edges_path, each_pred_edge_name)
+
+        # save 
+        
+        if not save_edge:
+
+            msk_pred = flip_inference(net, img_tensor, flip=flip, with_edge=save_edge)
+            msk_prednum = torch.sigmoid(msk_pred).squeeze()
+            msk_prednum[msk_prednum > 0.5] = 255
+            msk_prednum[msk_prednum <= 0.5] = 0
+
+            msk_pred = np.array(msk_prednum.cpu().data, dtype=np.uint8)
+
+            # 保存图片
+            cv2.imwrite(each_pred_msk_path, msk_pred)
+    
+
+        else: 
+            msk_pred, edge_pred = flip_inference(net,img_tensor,flip=True, with_edge=save_edge)
+            msk_prednum = torch.sigmoid(msk_pred).squeeze()
+            msk_prednum[msk_prednum > 0.5] = 255
+            msk_prednum[msk_prednum <= 0.5] = 0
+
+            msk_pred = np.array(msk_prednum.cpu().data, dtype=np.uint8)
+
+            edge_prednum = torch.sigmoid(edge_pred).squeeze()
+            edge_prednum[edge_prednum > 0.5] = 255
+            edge_prednum[edge_prednum <= 0.5] = 0
+
+            edge_pred = np.array(edge_prednum.cpu().data, dtype=np.uint8)
+
+            # 保存图片
+            cv2.imwrite(each_pred_msk_path, msk_pred)
+            cv2.imwrite(each_pred_edge_path, edge_pred)
+
+    print("Predicting has been finished!")
+                
+
+
+
+if __name__ == "__main__":
+
+    predict("./config/edge_newsp_bfm_seg_hrnet.yaml", 
+            "./pth/best_model.pth",
+            "./data/Test/img", 
+            "./data/Test/msk_pred", 
+            save_edge = True, 
+            flip = False)
+    
